ca9cba7ea263885255ee33dfd1e0d17bfac963ef
yewton <yewton@gmail.com>
2024-07-02 20:48:26 +0900

    Spotless を導入する

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,20 @@
+plugins {
+  id("com.diffplug.spotless") version "6.25.0"
+}
+
 // IDE でソースダウンロードする用
 repositories {
-    mavenCentral()
+  mavenCentral()
+}
+
+spotless {
+  kotlin {
+    target("**/*.kt")
+    targetExclude("**/build/**/*.kt", "**/jooq/**/*.kt")
+    ktlint().setEditorConfigPath("$rootDir/.editorconfig")
+  }
+  kotlinGradle {
+    target("**/*.gradle.kts")
+    ktlint()
+  }
 }
