504aa1324fcf555214111012eefd993e3e78bcef
yewton <yewton@gmail.com>
2025-06-08 21:51:22 +0900

    build: JDK を自動解決出来るようにする

--- build-logic/commons/build.gradle.kts
+++ build-logic/commons/build.gradle.kts
@@ -6,4 +6,7 @@ plugins {
 dependencies {
   implementation(platform("net.yewton.petclinic.platform:plugins-platform"))
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin")
+  implementation(
+    "org.gradle.toolchains.foojay-resolver-convention:org.gradle.toolchains.foojay-resolver-convention.gradle.plugin",
+  )
 }
